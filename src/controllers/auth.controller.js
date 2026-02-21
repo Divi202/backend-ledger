fc577fa77066a458d28db7ee0231fcfbb7d66402
Divi202 <divya8217274@gmail.com>
2026-02-21 23:21:19 +0530

    feat: api endpoint /api/auth/register

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,54 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+/**
+ * - user register controller
+ * - POST /api/auth/register
+ */
+async function userRegisterController(req, res) {
+  const { email, password, name } = req.body;
+
+  const isExists = await userModel.findOne({
+    email: email,
+  });
+
+  if (isExists) {
+    return res.status(422).json({
+      message: "User already exists with email",
+      status: "failed",
+    });
+  }
+
+  const user = await userModel.create({
+    email,
+    password,
+    name,
+  });
+
+  // generate token
+  const token = jwt.sign(
+    {
+      userId: user._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "3d",
+    },
+  );
+
+  // set token in cookies
+  res.cookie("token", token);
+
+  res.status(201).json({
+    user: {
+      _id: user._id,
+      email: user.email,
+      name: user.name,
+    },
+    token,
+  });
+}
+
+module.exports = {
+  userRegisterController,
+};
