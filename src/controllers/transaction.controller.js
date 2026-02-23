f517dbff9db84545da5b29d9d2cdf567878b0e35
Divi202 <divya8217274@gmail.com>
2026-02-23 23:31:23 +0530

    feat: transaction & ledger api

---
+++ src/controllers/transaction.controller.js
@@ -0,0 +1,221 @@
+const mongoose = require("mongoose");
+const ledgerModel = require("../models/ledger.model");
+const accountModel = require("../models/account.model");
+const transactionModel = require("../models/transaction.model");
+
+async function createTransaction(req, res) {
+  /**
+   * 1. Validate request
+   */
+  const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
+
+  if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
+    return res.status(400).json({
+      message: "FromAccont, toAccount, amount and idempotencyKey are requried",
+    });
+  }
+
+  const fromUserAccount = await accountModel.findOne({
+    _id: fromAccount,
+  });
+
+  const toUserAccount = await accountModel.findOne({
+    _id: toAccount,
+  });
+
+  if (!fromUserAccount || !toUserAccount) {
+    return res.status(400).json({
+      message: "Invalid fromAccount or toAccount",
+    });
+  }
+
+  /**
+   * 2. Validate idempotency key
+   *  */
+
+  const isTransactionAlreadyExists = await transactionModel.findOne({
+    idempotencyKey: idempotencyKey,
+  });
+
+  if (isTransactionAlreadyExists) {
+    if (isTransactionAlreadyExists.status === "COMPLETED") {
+      return res.status(200).json({
+        message: "Transaction already processed",
+        transaction: isTransactionAlreadyExists,
+      });
+    }
+    if (isTransactionAlreadyExists.status === "PENDING") {
+      return res.status(200).json({
+        message: "Transaction is still processing",
+      });
+    }
+    if (isTransactionAlreadyExists.status === "FAILED") {
+      return res.status(500).json({
+        message: "Transaction processing",
+        transaction: isTransactionAlreadyExists,
+      });
+    }
+  }
+
+  /**
+   *  3. Check amount status
+   * */
+
+  if (
+    fromUserAccount.status !== "ACTIVE" ||
+    toUserAccount.status !== "ACTIVE"
+  ) {
+    return res.status(400).json({
+      message:
+        "Both fromAccount and toAccount must be ACTIVE to process transaction",
+    });
+  }
+
+  /**
+   *  4. Derive sender balance from ledger
+   * */
+  const balance = await fromUserAccount.getBalance();
+
+  if (balance < amount) {
+    return (
+      res.status(400),
+      json({
+        message: `Insufficient balance. Current balance is ${balance}. Requested amount is ${amount}`,
+      })
+    );
+  }
+  /**
+   *  5. Create transaction (PENDING)
+   * */
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  const transaction = await transactionModel.create(
+    {
+      fromAccount,
+      toAccount,
+      amount,
+      idempotencyKey,
+      status: "PENDING",
+    },
+    {
+      session,
+    },
+  );
+  const debitLedgerEntry = await ledgerModel.create(
+    {
+      account: fromAccount,
+      amount: amount,
+      transaction: transaction._id,
+      type: "DEBIT",
+    },
+    { session },
+  );
+  const creditLedgerEntry = await ledgerModel.create(
+    {
+      account: toAccount,
+      amount: amount,
+      transaction: transaction._id,
+      type: "CREDIT",
+    },
+    { session },
+  );
+
+  transaction.status = "COMPLETED";
+  await transaction.save({ session });
+
+  session.endSession();
+
+  /**
+   *  10. Send Email notification
+   * */
+
+  await emailService.sendTransactionEmail(
+    req.user.email,
+    req.user.name,
+    amount,
+    toAccount,
+  );
+  return res.status(201).json({
+    message: "Transaction completed successfullly",
+    transaction: transaction,
+  });
+}
+
+async function createInitialFundsTransaction(req, res) {
+  const { toAccount, amount, idempotencyKey } = req.body;
+
+  if (!toAccount || !amount || !idempotencyKey) {
+    return res.status(400).json({
+      message: "ToAccount, amount and idempotencyKey are requried",
+    });
+  }
+
+  const toUserAccount = await accountModel.findOne({
+    _id: toAccount,
+  });
+
+  if (!toUserAccount) {
+    return res.status(400).json({
+      message: "Invalid toAccount",
+    });
+  }
+
+  const fromUserAccount = await accountModel.findOne({
+    user: req.user._id,
+  });
+
+  if (!fromUserAccount) {
+    return res.status(400).json({
+      message: "system user account not found",
+    });
+  }
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  const transaction = new transactionModel({
+    fromAccount: fromUserAccount._id,
+    toAccount,
+    amount,
+    idempotencyKey,
+    status: "PENDING",
+  });
+  const debitLedgerEntry = await ledgerModel.create(
+    [
+      {
+        account: fromUserAccount._id,
+        amount: amount,
+        transaction: transaction._id,
+        type: "DEBIT",
+      },
+    ],
+    { session },
+  );
+  const creditLedgerEntry = await ledgerModel.create(
+    [
+      {
+        account: toAccount,
+        amount: amount,
+        transaction: transaction._id,
+        type: "CREDIT",
+      },
+    ],
+    { session },
+  );
+
+  transaction.status = "COMPLETED";
+  await transaction.save({ session });
+
+  session.endSession();
+
+  return res.status(201).json({
+    message: "Initial funds transaction completed successfully",
+    transaction: transaction,
+  });
+}
+
+module.exports = {
+  createTransaction,
+  createInitialFundsTransaction,
+};
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const ledgerModel = require("./ledger.model");
 
 const accountSchema = new mongoose.Schema(
   {
@@ -29,6 +30,51 @@ const accountSchema = new mongoose.Schema(
 
 //Compond Index
 accountSchema.index({ user: 1, status: 1 });
+
+accountSchema.methods.getBalance = async function () {
+  const balanceData = await ledgerModel.aggregate([
+    {
+      $match: { account: this_id },
+    },
+    {
+      $group: {
+        _id: null,
+        totalDebit: {
+          $sum: {
+            $cond: [
+              {
+                $eq: ["$type", "DEBIT"],
+              },
+              "$amount",
+              0,
+            ],
+          },
+        },
+        totalCredit: {
+          $sum: {
+            $cond: [
+              {
+                $eq: ["$type", "CREDIT"],
+              },
+              "$amount",
+              0,
+            ],
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        balance: { $subtracts: ["$totalCredit", "totalDebit"] },
+      },
+    },
+  ]);
+  if (balanceData.length === 0) {
+    return 0;
+  }
+  return balanceData[0].balance;
+};
 
 const accountModel = mongoose.model("account", accountSchema);
 
