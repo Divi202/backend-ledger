6209a997c803864aed26b77eee7657ba6522fab5
Divi202 <divya8217274@gmail.com>
2026-02-24 17:47:19 +0530

    feat: add health check endpoint for ledger service

--- src/app.js
+++ src/app.js
@@ -17,6 +17,9 @@ const transactionRouter = require("./routes/transaction.route");
 /**
  * - Use Routes
  */
+app.get("/", (req, res) => {
+  res.send("Ledger Service is up and running");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/accounts", accountRouter);
 app.use("/api/transactions", transactionRouter);
